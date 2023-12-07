6e9ff6bebbed53f87805aef0784125fb4c2eff74
aothymoon59 <aothymoon59@gmail.com>
2023-12-07 21:40:51 +0600

    Test Academic Faculty Routes using Postman

--- src/app/modules/academicFaculty/academicFaculty.validation.ts
+++ src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -1,14 +1,18 @@
 import { z } from 'zod';
 
 const createAcademicFacultyValidationSchema = z.object({
-  name: z.string({
-    invalid_type_error: 'Academic Faculty must be string',
+  body: z.object({
+    name: z.string({
+      invalid_type_error: 'Academic Faculty must be string',
+    }),
   }),
 });
 
 const UpdateAcademicFacultyValidationSchema = z.object({
-  name: z.string({
-    invalid_type_error: 'Academic Faculty must be string',
+  body: z.object({
+    name: z.string({
+      invalid_type_error: 'Academic Faculty must be string',
+    }),
   }),
 });
 
