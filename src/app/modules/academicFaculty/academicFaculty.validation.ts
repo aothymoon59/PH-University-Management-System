5ef9ec11d816dea551e26333e49b5f3cc3486e08
aothymoon59 <aothymoon59@gmail.com>
2023-12-07 18:22:03 +0600

    AcademicFaculty validation created

---
+++ src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+const academicFacultyValidationSchema = z.object({
+  name: z.string({
+    invalid_type_error: 'Academic Faculty must be string',
+  }),
+});
+
+export const academicFacultyValidation = {
+  academicFacultyValidationSchema,
+};
