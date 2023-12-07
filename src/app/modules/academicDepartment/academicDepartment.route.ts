c103eaf7e86d70b9411db0f4cc918ea89cece6db
aothymoon59 <aothymoon59@gmail.com>
2023-12-08 01:08:57 +0600

    Create Academic Department controller , route and test using postman

---
+++ src/app/modules/academicDepartment/academicDepartment.route.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { academicDepartmentValidations } from './academicDepartment.validation';
+import { AcademicDepartmentControllers } from './academicDepartment.controller';
+
+const router = express.Router();
+
+router.post(
+  '/create-academic-department',
+  validateRequest(
+    academicDepartmentValidations.createAcademicDepartmentValidationSchema,
+  ),
+  AcademicDepartmentControllers.createAcademicDepartment,
+);
+
+router.get('/', AcademicDepartmentControllers.getAllAcademicDepartments);
+
+router.get(
+  '/:departmentId',
+  AcademicDepartmentControllers.getSingleAcademicDepartment,
+);
+
+router.patch(
+  '/:departmentId',
+  validateRequest(
+    academicDepartmentValidations.UpdateAcademicDepartmentValidationSchema,
+  ),
+  AcademicDepartmentControllers.updateAcademicDepartment,
+);
+
+export const AcademicDepartmentRoutes = router;
