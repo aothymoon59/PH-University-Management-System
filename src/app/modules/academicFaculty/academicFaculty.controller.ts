214126ce1b8f308d2e3ff3d34aa9f8f9c5b81fa4
aothymoon59 <aothymoon59@gmail.com>
2023-12-07 19:14:19 +0600

    Create Academic faculty routes, controllers and services

---
+++ src/app/modules/academicFaculty/academicFaculty.controller.ts
@@ -0,0 +1,66 @@
+/* eslint-disable no-undef */
+import httpStatus from 'http-status';
+import sendResponse from '../../utils/sendResponse';
+import catchAsync from '../../utils/catchAsync';
+import { AcademicFacultyServices } from './academicFaculty.service';
+
+const createAcademicFaculty = catchAsync(async (req, res) => {
+  const result = await AcademicFacultyServices.createAcademicFacultyIntoDB(
+    req.body,
+  );
+
+  // send response
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Faculty is created successfully',
+    data: result,
+  });
+});
+
+const getAllAcademicFaculties = catchAsync(async (req, res) => {
+  const result = await AcademicFacultyServices.getAllAcademicFacultiesFromDB();
+
+  // send response
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Faculties are retrieved successfully',
+    data: result,
+  });
+});
+
+const getSingleAcademicFaculty = catchAsync(async (req, res) => {
+  const { facultyId } = req.params;
+  const result =
+    await AcademicFacultyServices.getSingleAcademicFacultyFromDB(facultyId);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic faculty is retrieved successfully',
+    data: result,
+  });
+});
+
+const updateAcademicFaculty = catchAsync(async (req, res) => {
+  const { facultyId } = req.params;
+  const result = await AcademicFacultyServices.updateAcademicFacultyFromDB(
+    facultyId,
+    req.body,
+  );
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Faculty is updated successfully',
+    data: result,
+  });
+});
+
+export const AcademicFacultyControllers = {
+  createAcademicFaculty,
+  getAllAcademicFaculties,
+  getSingleAcademicFaculty,
+  updateAcademicFaculty,
+};
---
+++ src/app/modules/academicFaculty/academicFaculty.route.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { academicFacultyValidation } from './academicFaculty.validation';
+import { AcademicFacultyControllers } from './academicFaculty.controller';
+
+const router = express.Router();
+
+router.post(
+  '/create-academic-faculty',
+  validateRequest(
+    academicFacultyValidation.createAcademicFacultyValidationSchema,
+  ),
+  AcademicFacultyControllers.createAcademicFaculty,
+);
+
+router.get('/', AcademicFacultyControllers.getAllAcademicFaculties);
+
+router.get('/:facultyId', AcademicFacultyControllers.getSingleAcademicFaculty);
+
+router.patch(
+  '/:facultyId',
+  validateRequest(
+    academicFacultyValidation.UpdateAcademicFacultyValidationSchema,
+  ),
+  AcademicFacultyControllers.updateAcademicFaculty,
+);
+
+export const AcademicFacultyRoutes = router;
