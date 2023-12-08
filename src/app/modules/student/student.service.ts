af44188b7e9370267e2eee08e5066f0d7c3ee8e5
aothymoon59 <aothymoon59@gmail.com>
2023-12-09 01:15:19 +0600

    Implement logic to handle dynamically update non-primitive fields

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -30,8 +30,35 @@ const getSingleStudentFromDB = async (id: string) => {
   return result;
 };
 
-const updateStudentIntoDB = async (id: string, payLoad: Partial<TStudent>) => {
-  const result = await Student.findOneAndUpdate({ id }, payLoad);
+const updateStudentIntoDB = async (id: string, payload: Partial<TStudent>) => {
+  const { name, guardian, localGuardian, ...remainingStudentData } = payload;
+
+  const modifiedUpdatedData: Record<string, unknown> = {
+    ...remainingStudentData,
+  };
+
+  if (name && Object.keys(name).length) {
+    for (const [key, value] of Object.entries(name)) {
+      modifiedUpdatedData[`name.${key}`] = value;
+    }
+  }
+
+  if (guardian && Object.keys(guardian).length) {
+    for (const [key, value] of Object.entries(guardian)) {
+      modifiedUpdatedData[`guardian.${key}`] = value;
+    }
+  }
+
+  if (localGuardian && Object.keys(localGuardian).length) {
+    for (const [key, value] of Object.entries(localGuardian)) {
+      modifiedUpdatedData[`localGuardian.${key}`] = value;
+    }
+  }
+
+  const result = await Student.findOneAndUpdate({ id }, modifiedUpdatedData, {
+    new: true,
+    runValidators: true,
+  });
   return result;
 };
 
