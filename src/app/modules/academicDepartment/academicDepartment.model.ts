16b88c870bf1f87f8c78186969b18874fdac16ff
aothymoon59 <aothymoon59@gmail.com>
2023-12-12 01:03:14 +0600

    handle castError and 11000 error

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -20,7 +20,7 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>(
   },
 );
 
-/* academicDepartmentSchema.pre('save', async function (next) {
+academicDepartmentSchema.pre('save', async function (next) {
   const isDepartmentExist = await AcademicDepartment.findOne({
     name: this.name,
   });
@@ -33,7 +33,7 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>(
   }
 
   next();
-}); */
+});
 
 academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
   const query = this.getQuery();
