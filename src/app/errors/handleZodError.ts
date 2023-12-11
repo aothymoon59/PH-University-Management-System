9d0f18fc62c713b265623e8f101037b3d10beb86
aothymoon59 <aothymoon59@gmail.com>
2023-12-12 00:20:49 +0600

    handle error in mongoose

---
+++ src/app/errors/handleZodError.ts
@@ -0,0 +1,20 @@
+import { ZodError, ZodIssue } from 'zod';
+import { TErrorSources } from '../interface/error';
+
+const handleZodError = (err: ZodError) => {
+  const errorSources: TErrorSources = err.issues.map((issue: ZodIssue) => {
+    return {
+      path: issue?.path[issue.path.length - 1],
+      message: issue.message,
+    };
+  });
+
+  const statusCode = 400;
+
+  return {
+    statusCode,
+    message: 'Zod Validation error',
+    errorSources,
+  };
+};
+export default handleZodError;
