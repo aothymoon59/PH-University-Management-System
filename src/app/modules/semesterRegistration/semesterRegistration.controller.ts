c0b55daa9b32335559d212bde6c753f725151099
aothymoon59 <aothymoon59@gmail.com>
2023-12-24 23:57:21 +0600

    Apply Business Logic for creating and updating semester registration

---
+++ src/app/modules/semesterRegistration/semesterRegistration.controller.ts
@@ -0,0 +1,70 @@
+import httpStatus from 'http-status';
+import sendResponse from '../../utils/sendResponse';
+import { SemesterRegistrationService } from './semesterRegistration.service';
+import catchAsync from '../../utils/catchAsync';
+
+const createSemesterRegistration = catchAsync(async (req, res) => {
+  const result =
+    await SemesterRegistrationService.createSemesterRegistrationIntoDB(
+      req.body,
+    );
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Semester Registration is created successfully!',
+    data: result,
+  });
+});
+
+const getAllSemesterRegistrations = catchAsync(async (req, res) => {
+  const result =
+    await SemesterRegistrationService.getAllSemesterRegistrationsFromDB(
+      req.query,
+    );
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Semester Registration is retrieved successfully !',
+    data: result,
+  });
+});
+
+const getSingleSemesterRegistration = catchAsync(async (req, res) => {
+  const { id } = req.params;
+
+  const result =
+    await SemesterRegistrationService.getSingleSemesterRegistrationsFromDB(id);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Semester Registration is retrieved successfully',
+    data: result,
+  });
+});
+
+const updateSemesterRegistration = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const result =
+    await SemesterRegistrationService.updateSemesterRegistrationIntoDB(
+      id,
+      req.body,
+    );
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Semester Registration is updated successfully',
+    data: result,
+  });
+});
+
+export const SemesterRegistrationController = {
+  createSemesterRegistration,
+  getAllSemesterRegistrations,
+  getSingleSemesterRegistration,
+  updateSemesterRegistration,
+  //   deleteSemesterRegistration,
+};
