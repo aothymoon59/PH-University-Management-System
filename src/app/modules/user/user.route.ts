4beebc4bcc1314817c15b90744295b3a3441e1b3
aothymoon59 <aothymoon59@gmail.com>
2023-12-26 13:36:30 +0600

    Implement authorization using auth

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -4,24 +4,29 @@ import validateRequest from '../../middlewares/validateRequest';
 import { StudentValidations } from '../student/student.validation';
 import { facultyValidations } from '../Faculty/faculty.validation';
 import { createAdminValidationSchema } from '../Admin/admin.validation';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from './user.constant';
 
 const router = express.Router();
 
 // will call controller func
 router.post(
   '/create-student',
+  auth(USER_ROLE.admin),
   validateRequest(StudentValidations.createStudentValidationSchema),
   UserControllers.createStudent,
 );
 
 router.post(
   '/create-faculty',
+  auth(USER_ROLE.admin),
   validateRequest(facultyValidations.createFacultyValidationSchema),
   UserControllers.createFaculty,
 );
 
 router.post(
   '/create-admin',
+  // auth(USER_ROLE.admin),
   validateRequest(createAdminValidationSchema),
   UserControllers.createAdmin,
 );
